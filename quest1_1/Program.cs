55cd242644b32f23f5a6efb9f13218acc0697909
Ivan <tornadod@mail.ru>
2022-02-01 19:24:47 +0300

    часть задания 1_1(1-2) выполнена

---
+++ quest1_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace quest1_1
+{
+    class Program
+    {
+        static bool SimpleNumber(int number)
+        {
+            int d = 0;
+            bool simple = false;
+            for (int i = 2; i < number; i++)
+            {
+                if (i < number)
+                {
+                    if (number % i == 0) d++;
+                }
+            }
+            if (d == 0) simple = true;
+            return simple;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Input number: ");
+            n = Int32.Parse(Console.ReadLine());
+            SimpleNumber(n);
+        }
+    }
+}
